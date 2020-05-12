dc00046a21c541515034f86a13dd2f9ed0252e08
Thomas J  Shotts Jr <37753391+darkwolfxj@users.noreply.github.com>
2020-05-12 10:42:45 -0400

    initial commit

---
+++ api/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const server = express()
+server.use("/", express.json())
+
+server.get("/", (req, res) => {
+    res.status(200).json({ message: "up" })
+})
+module.exports = server;
---
+++ plan.md
@@ -0,0 +1,3 @@
+1. find some way to grab now playing track id
+2. use python/javascript to use that track id to make calls to Deezer API
+3. dynamically implement those calls into rich presence
