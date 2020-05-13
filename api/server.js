ec3852667901ac6e1c6fa86e2f0be4d343371510
Thomas J  Shotts Jr <37753391+darkwolfxj@users.noreply.github.com>
2020-05-13 19:56:23 -0400

    got the api working fully, now it's time to implement a token getting function with pyhton, and then send it to the /token endpoint in the api, but only when it's needed

--- api/server.js
+++ api/server.js
@@ -1,34 +1,91 @@
 const express = require("express");
 const server = express();
 const RPC = require("discord-rpc");
+const axios = require("axios");
 
-const start = Date.now();
-const currentSong = "Strange Terrain";
-const currentArtist = "Circa Survive";
-const currentSongLength = 243;
-const endTimeStamp = Date.now() + currentSongLength;
- 
+//Initialze Rich Presence Variables
+let now = Date.now(),
+access_token,
+song_title,
+album_title,
+artist_name;
+
+//Gets user listening history and sets Rich Presence Variables based on the most recently played song.
+const getSong = () => {
+    return axios
+        .get(`http://api.deezer.com/user/me/history?access_token=${ access_token }&index=0&limit=1`)
+        .then(res => {
+            return axios
+                .get(`https://api.deezer.com/track/${ res.data.data[0].id }`)
+                .then(res => {
+                    song_title = res.data.title,
+                    album_title = res.data.album.title,
+                    artist_name = res.data.artist.name;
+                });
+        });
+}
+
+// Functions which return the data used in rich presence update
+const details = () => `Recently listened to : ${ song_title }`
+const state = () => `by ${ artist_name }, on ${ album_title }`;
+
+// Function which sets the user's rich presence based on listening history from the user's Deezer account
+const setActivity = async () => {
+            await client.setActivity({
+                details: details(),
+                state: state(),
+                startTimestamp: now,
+                largeImageKey: "deezer_logo",
+                smallImageKey: "null",
+                instance: false,
+            })
+                .then(() => console.log(`set Activity, song title: ${ song_title }, album title: ${ album_title }, artist name: ${ artist_name }`))
+                .catch(err => console.error(err));
+};
+
+// Sets up the rich presence connection from this api to the user's Discord account
 const client = new RPC.Client({ transport: 'ipc' });
 
+// Function that dicord-rpc runs in order to set rich presence for the user
+const wait = () => { 
+    if (typeof(access_token) === "undefined"){
+        console.log("waiting for token");
+        setTimeout(wait, 5000);
+    } else {
+        getSong()
+            .then(() => {
+                setActivity();
+                setInterval(() => {
+                    getSong()
+                        .then(() => {
+                            setActivity();
+                            setTimeout(getSong, 15000);
+                        });
+                }, 15000)
+            });
+    };
+};
 client.on("ready", () => {
-    setInterval(() => client.setActivity({
-        details: `Listening to: ${ currentSong } (this is a placeholder; soon I will render the currently playing song)`,
-        state: `by ${ currentArtist } (another placeholder; soon to be the currently playing artist)`,
-        startTimestamp: start,
-        largeImageKey: `${ process.env.LARGE_IMAGE_KEY }`,
-        smallImageKey: "null",
-        instance: false,
-    }), currentSongLength)
-})
-
+    wait();
+});
  
 // Log in to RPC with client id
-client.login({ clientId: `${ process.env.CLIENT_ID }`, clientSecret: `${ process.env.CLIENT_SECRET }` }).catch((error) => console.error("error logging in", error));  
+// Initializes the rich presence connection from this api to the user's Discord account
+client.login({ clientId: "709635687820820520"}).catch((error) => console.error("error logging in", error));  
+
+server.use(express.json())
 
 server.get("/", (req, res) => {
-    res.status(200).json({ message: "up" })
+    res.status(200).json({ message: "up" });
 })
+
+server.post("/token", (req, res) => {
+    access_token = req.body.token;
+    res.status(200).json({ message: "token recieved" });
+})
+
+// API endpoint for Auth redirect URI 
 server.get("/redirect", (req, res) => {
-    res.status(200).sendFile(__dirname + "/index.html")
+    res.status(200).sendFile(__dirname + "/index.html");
 })
 module.exports = server;
---
+++ bot.py
@@ -0,0 +1,5 @@
+import sys
+from selenium import webdriver
+from pynput.keyboard import Key, Controllers
+
+from secrets.py import username, password
